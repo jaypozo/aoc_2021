1295381682fc8c07e83ffdc65be6bb20de99a092
Jay Pozo <jaypozo@gmail.com>
2021-12-06 17:10:23 -0800

    working 01

---
+++ 01/index.ts
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const readline = require('readline')
+
+let counter = 0
+let prevValue = undefined
+
+let stream = fs.createReadStream('./01/input.txt')
+const rl = readline.createInterface({
+    input: stream,
+})
+
+rl.on('line', (line) => {
+    let lineInt = parseInt(line)
+    if ((prevValue) && (prevValue < lineInt)) counter++
+    prevValue = line
+})
+
+rl.on('close', () => {console.log(counter)})
